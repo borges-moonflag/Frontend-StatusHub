8a4140cfa3e45fd06c5134c9f3f6349aff46017d
borges-moonflag <borges@agmoonflag.com.br>
2025-10-01 17:47:38 -0300

    fix: error rendering too many times

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -29,7 +29,7 @@ const onSubmit = async (data) => {
   }
 };
 
-    const FetchApiTeste  = async () {
+    const FetchApiTeste  = async () => {
     const res = await fetch("/api/test");
     const data = await res.json();
     console.log(data);
